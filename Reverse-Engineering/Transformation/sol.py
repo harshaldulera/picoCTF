87c06fbc9356c236a0b58cc89eabec47203af671
Harshal Dulera <harshaldulera82@gmail.com>
2024-03-02 23:35:39 +0530

    Rev: Transformation

---
+++ Reverse-Engineering/Transformation/sol.py
@@ -0,0 +1,10 @@
+encoded = open('enc').read()
+flag = ""
+
+for i in range(0, len(encoded)):
+    char1 = chr((ord(encoded[i]) >> 8))
+    char2 = chr(encoded[i].encode('utf-16be')[-1])
+    flag += char1
+    flag += char2
+
+print(flag)
---
+++ Reverse-Engineering/Transformation/solution.md
@@ -0,0 +1,8 @@
+```
+┌──(kali㉿kali)-[~/Desktop/picoctf]
+└─$ cat enc 
+灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸彤㔲挶戹㍽
+```
+
+Writing a python script.
+
